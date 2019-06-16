8ab803b2be5b741ce01b1d205be76b7ab6d3db83
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2019-06-17 03:06:37 +0530

    User model

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const logger = require('morgan');
 
+const users = require('./routes/users')
+
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.use(logger('dev'));
+
+app.use('/users', users);
 
 // Catch 404 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err)
-})
+});
 
 // Error handler
 app.use((err, req, res, next) => {
     res.status(500).json({
         message: err.message
     })
-})
+});
 
-app.listen(port, () => console.log(`Server started at ${port}`))
+app.listen(port, () => console.log(`Server started at ${port}`));
